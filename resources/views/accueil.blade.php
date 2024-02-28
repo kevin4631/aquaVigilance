950dd75e99767cc26cb44dc353aaf6195f964779
bouziane seddak <bouzhichem31@gmail.com>
2024-02-28 13:13:14 +0100

    carte

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -5,22 +5,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> AquaVigilance </title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+    <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/Leaflet.river.js"></script>
 </head>
 
 <style>
-   
+    html,
+        body {
+            height: 91.5%;
+            margin: 0;
+        }
     h1{
         text-align: center;
         font-size: 80px;
     }
+    .leaflet-container {
+            height: 400px;
+            width: 600px;
+            max-width: 100%;
+            max-height: 100%;
+            margin: 0 auto;
+        }
 
+        #map {
+            height: 100%;
+            width: 60%;
+        }
 </style>
 <body>
     @include('headfoot/header')
 
-    <main class="text_center">
-        <h1 class="titre2"> à faire :/</h1>
-    </main>
+    <div id="map">
+        <script>
+            const map = L.map("map").setView([46.6031, 1.8883], 6);
+            const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+        </script>
+    </div>
 
     @include('headfoot/footer')
 </body>
