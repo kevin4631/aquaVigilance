a29cbb590cf03544dab9cb7cc33be097055e176b
Juba <abbacijuba@gmail.com>
2024-04-06 14:14:58 +0200

    Algo de classement

--- resources/views/statistiques/classement.blade.php
+++ resources/views/statistiques/classement.blade.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Classement </title>
+    <title> Classement des régions </title>
 </head>
 <body>
-    
+<?php
+
+// Inclure le fichier getDelta.php
+include 'getDelta.php';
+
+$liste_code_cours_eau = [];
+$liste_code_cours_eau = sql::select("SELECT code_cours_eau FROM cours_eau;");
+
+
+
+
+
+?>
 </body>
 </html>
