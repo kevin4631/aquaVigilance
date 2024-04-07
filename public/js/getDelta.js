ea3b5a4c2ac93ac54feb01c4c5e042086a059418
Juba <abbacijuba@gmail.com>
2024-04-07 15:50:23 +0200

    Proposition fonction classement

--- public/js/getDelta.js
+++ public/js/getDelta.js
@@ -21,3 +21,53 @@ function getDelta(annee1, annee2, code_cours_eau) {
         xhr.send(params);
     });
 }
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+// Définir les années
+const annee1 = 2019;
+const annee2 = 2020;
+
+// Créer un tableau pour stocker les deltas des cours d'eau
+const deltas_cours_eau = [];
+
+// Fonction pour récupérer les codes des cours d'eau depuis le serveur
+function getCodesCoursEau() {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject(new Error('Une erreur s\'est produite lors de la récupération des codes des cours d\'eau. Statut de la requête: ' + this.status));
+                }
+            }
+        };
+        xhr.open('GET', 'php/getCodesCoursEau.php', true);
+        xhr.send();
+    });
+}
+
+// Fonction pour obtenir les deltas pour chaque cours d'eau et les stocker dans le tableau
+async function getDeltasForCoursEau() {
+    try {
+        // Récupérer les codes des cours d'eau depuis le serveur
+        const codes_cours_eau = await getCodesCoursEau();
+
+        // Pour chaque code de cours d'eau, récupérer le delta et le stocker dans le tableau
+        for (const code_cours_eau of codes_cours_eau) {
+            const delta = await getDelta(annee1, annee2, code_cours_eau);
+            deltas_cours_eau.push({ code_cours_eau, delta });
+            console.log(`Delta pour ${code_cours_eau} : ${delta}`);
+        }
+        console.log("Deltas des cours d'eau:", deltas_cours_eau);
+    } catch (error) {
+        console.error('Une erreur s\'est produite lors de la récupération des deltas des cours d\'eau:', error);
+    }
+}
+
+// Appeler la fonction pour obtenir les deltas pour chaque cours d'eau
+getDeltasForCoursEau();
+
+
+
