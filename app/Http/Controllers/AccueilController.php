da75bc459183d1944c16dbe1f215512bf4b651e6
Juba <abbacijuba@gmail.com>
2024-04-08 18:34:22 +0200

    Avancement Classement

--- app/Http/Controllers/AccueilController.php
+++ app/Http/Controllers/AccueilController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AccueilController extends Controller
 {
@@ -15,42 +16,40 @@ class AccueilController extends Controller
     public function classement() {
 
         // Tableau associatif des codes de région
-$regions = [
-    'guadeloupe' => 1,
-    'ile_france' => 11,
-    'martinique' => 2,
-    'centre_val_loire' => 24,
-    'bourgogne_franche_compte' => 27,
-    'normandie' => 28,
-    'guyane' => 3,
-    'hauts_france' => 32,
-    'reunion' => 4,
-    'grand_est' => 44,
-    'pays_loire' => 52,
-    'bretagne' => 53,
-    'mayotte' => 6,
-    'nouvelle_aquitaine' => 75,
-    'occitanie' => 76,
-    'auvergne_rhone_alpes' => 84,
-    'paca' => 93,
-    'corse' => 94
-];
+        $regions = [
+            'guadeloupe' => 1,
+            'ile_france' => 11,
+            'martinique' => 2,
+            'centre_val_loire' => 24,
+            'bourgogne_franche_compte' => 27,
+            'normandie' => 28,
+            'guyane' => 3,
+            'hauts_france' => 32,
+            'reunion' => 4,
+            'grand_est' => 44,
+            'pays_loire' => 52,
+            'bretagne' => 53,
+            'mayotte' => 6,
+            'nouvelle_aquitaine' => 75,
+            'occitanie' => 76,
+            'auvergne_rhone_alpes' => 84,
+            'paca' => 93,
+            'corse' => 94
+        ];
 
-// Tableau pour stocker les résultats
-$cours_eau = [];
+        // Tableau pour stocker les résultats
+        $cours_eau = [];
 
-// Boucle pour récupérer les cours d'eau pour chaque région
-foreach ($regions as $region => $code_region) {
-    $cours_eau[$region] = DB::select("SELECT code_cours_eau 
-                                       FROM association_region_eau
-                                       WHERE code_region = $code_region;");
-}
-
-
+        // Boucle pour récupérer les cours d'eau pour chaque région
+        foreach ($regions as $region => $code_region) {
+            $cours_eau[$code_region] = DB::select("SELECT code_cours_eau 
+                                            FROM association_region_eau
+                                            WHERE code_region = $code_region;");
+        }
 
 
-    return view("statistiques/classement", ['cours_eau' => $cours_eau]);
-    }
+            return view("statistiques/classement", ['cours_eau' => $cours_eau]);
+            }
 
 
 }
--- resources/views/statistiques/classement.blade.php
+++ resources/views/statistiques/classement.blade.php
@@ -3,28 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Classement des régions </title>
+    <title>Classement des régions</title>
 </head>
 <body>
-<?php
+    <script src="js/evolution.js"></script>
 
-
-
-
- 
-?>
-
-
-<script src="js/evolution.js"></script>
-<script src="js/getAllCodeCoursEau.js"></script>
+    <!--------------- recupere tous les codes cours eau --------------->
+    <script src="js/getAllCodeCoursEau.js"></script>
+    
     <script>
+        // Récupération des données PHP dans des variables JavaScript
+        var coursEau = <?php echo json_encode($cours_eau); ?>;
+        console.log(coursEau);
+
+        // recup les codes cours eau
         var tab_codeCoursEau = getAllCodeCoursEau();
-        getTabDelta(2010, 2020, tab_codeCoursEau).then(function(tab_delta) {
-        console.log(tab_delta);
-        }).catch(function(error) {
-            console.error(error);
+
+        // Supposons que coursEau[11] soit le sous-tableau dont vous voulez extraire les valeurs
+        var sousTableau = coursEau[11];
+
+        // Utilisation de la méthode map pour extraire les valeurs de chaque objet
+        var ce_ileFrance = sousTableau.map(function(objet) {
+            return objet.code_cours_eau; // Extraction de la valeur de la clé "code_cours_eau"
         });
 
-        </script>
+        console.log(ce_ileFrance);
+
+        var somme_delta_ile_france = 0;
+        getTabDelta(2010, 2020, ce_ileFrance).then(function(tab_delta) {
+                console.log(tab_delta[0]);
+                for (var i = 0; i < tab_delta.length; i++) {
+                    if (tab_delta[i].delta != 999) {
+                        somme_delta_ile_france += parseInt(tab_delta[i].delta,10);
+                    }
+                }
+                console.log(somme_delta_ile_france);
+            }).catch(function(error) {
+                console.error(error);
+            });
+
+
+    </script>
 </body>
 </html>
