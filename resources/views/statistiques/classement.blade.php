775c6786c7126042af43b9f05b556fc6a9c7c501
kevin <kevinrochetaing46@gmail.com>
2024-04-08 21:12:12 +0200

    page evo

--- resources/views/statistiques/classement.blade.php
+++ resources/views/statistiques/classement.blade.php
@@ -1,70 +1,105 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classement des régions</title>
 </head>
+
 <body>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="js/evolution.js"></script>
 
-    <!--------------- recupere tous les codes cours eau --------------->
-    <script src="js/getAllCodeCoursEau.js"></script>
-    
     <script>
-        // Récupération des données PHP dans des variables JavaScript
-        var coursEau = <?php echo json_encode($cours_eau); ?>;
-        console.log(coursEau);
+        const regions = {
+            11: "Île-de-France",
+            24: "Centre-Val de Loire",
+            27: "Bourgogne-Franche-Comté",
+            28: "Normandie",
+            32: "Hauts-de-France",
+            44: "Grand Est",
+            52: "Pays de la Loire",
+            53: "Bretagne",
+            75: "Nouvelle-Aquitaine",
+            76: "Occitanie",
+            84: "Auvergne-Rhône-Alpes",
+            93: "Provence-Alpes-Côte d'Azur",
+            94: "Corse"
+        };
 
-        // recup les codes cours eau
-        var tab_codeCoursEau = getAllCodeCoursEau();
 
-      // Initialisation d'un objet pour stocker les sommes des deltas par région
-var sommes_deltas_par_region = {};
+        var annee1 = 2010;
+        var annee2 = 2020;
 
-// Boucle à travers chaque région
-for (var region in coursEau) {
-    if (coursEau.hasOwnProperty(region)) {
-        var sousTableau = coursEau[region];
-        var ce_region = sousTableau.map(function(objet) {
-            return objet.code_cours_eau; // Extraction de la valeur de la clé "code_cours_eau"
-        });
+        var tab_delta = [];
+        var tab_Region = [];
 
-        // Initialisation de la somme des deltas et du compteur de cours d'eau pour la région actuelle
-        var somme_delta_region = 0;
-        var nombre_cours_eau_region = 0;
+        getDeltaRegion(annee1, annee2).then(function(tab_deltaRegion) {
+            console.log(tab_deltaRegion);
 
-        // Calcul de la somme des deltas pour la région actuelle
-        getTabDelta(2010, 2020, ce_region).then(function(tab_delta) {
-            for (var i = 0; i < tab_delta.length; i++) {
-                if (tab_delta[i].delta != 999) {
-                    somme_delta_region += parseInt(tab_delta[i].delta, 10);
-                    nombre_cours_eau_region++; // Incrémentation du compteur de cours d'eau
-                }
-            }
-            // Calcul de la moyenne des deltas pour la région et stockage dans l'objet
-            sommes_deltas_par_region[region] = somme_delta_region / nombre_cours_eau_region;
+            tab_deltaRegion.forEach(deltaRegion => {
+                tab_Region.push(regions[deltaRegion[0]]);
+                tab_delta.push(deltaRegion[1]);
+            });
+            console.log(tab_delta);
+            console.log(tab_Region);
 
-            console.log("Moyenne des deltas pour la région : " + sommes_deltas_par_region[region]);
+            drawGraphique();
         }).catch(function(error) {
             console.error(error);
         });
-    }
-}
 
 
-// Calcul de la moyenne des deltas pour chaque région
-for (var region in sommes_deltas_par_region) {
-    if (sommes_deltas_par_region.hasOwnProperty(region)) {
-        var somme_delta_region = sommes_deltas_par_region[region];
-        var nombre_cours_eau = coursEau[region].length;
-        var moyenne_delta_region = somme_delta_region / nombre_cours_eau;
-        console.log("Moyenne des deltas pour la région: " + moyenne_delta_region);
-    }
-}
+        function drawGraphique() {
+            const ctx = document.getElementById('myChart');
 
+            const data = {
+                labels: tab_Region,
+                datasets: [{
+                    label: 'delta',
+                    data: tab_delta,
+                    borderColor: 'rgba(255,0,0,1)',
+                    backgroundColor: 'rgba(255,0,0,0.5)',
+                }]
+            };
 
-
+            new Chart(ctx, {
+                type: 'bar',
+                data: data,
+                options: {
+                    indexAxis: 'y',
+                    // Elements options apply to all of the options unless overridden in a dataset
+                    // In this case, we are setting the border of each horizontal bar to be 2px wide
+                    elements: {
+                        bar: {
+                            borderWidth: 2,
+                        }
+                    },
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            position: 'right',
+                        },
+                        title: {
+                            display: true,
+                            text: 'evolution'
+                        }
+                    },
+                    scales: {
+                        x: {
+                            min: -5,
+                            max: 5
+                        }
+                    }
+                },
+            });
+        }
     </script>
 </body>
+
 </html>
