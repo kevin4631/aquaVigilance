33207d1b77b6f1be629cdd3cc233da73eb39c1de
kevin <kevinrochetaing46@gmail.com>
2024-04-08 08:00:09 +0200

    mise au propre css

--- resources/views/page/temperature/historique_temperature.blade.php
+++ resources/views/page/temperature/historique_temperature.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,38 +9,34 @@
     <link rel="shortcut icon" type="image/x-icon" href="img/logo.png" />
 
     <link rel="stylesheet" href="css/historique.css">
-    <link rel="stylesheet" href="css/footer.css"/>
+    <link rel="stylesheet" href="css/footer.css" />
     <link rel="stylesheet" href="css/header.css" />
 
 </head>
+
 <body>
     @include('headfoot/header')
 
     <h1>Historique des Températures</h1>
-    <div class="button-container">
-        <a href="{{route('accueil')}}" class="accueil-btn">accueil</a>
-        <a href="{{route('temp_formulaire')}}" class="temperature-btn">Page Temperature</a>
-        <a href="{{route('deconnexion')}}" class="deco-btn">Déconnexion</a>
-    </div>
 
     <div class="container">
-        
-    @if($temperatures->isEmpty())
-    <p>Aucune donnée de température trouvée.</p>
-@else
-    <table>
-        <thead>
-            <tr>
-                <th>Date de mesure</th>
-                <th>Longitude</th>
-                <th>Latitude</th>
-                <th>Commune</th>
-                <th>Cours d'eau</th>
-                <th>Temperature en C°</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($temperatures as $temperature)
+
+        @if($temperatures->isEmpty())
+        <p>Aucune donnée de température trouvée.</p>
+        @else
+        <table>
+            <thead>
+                <tr>
+                    <th>Date de mesure</th>
+                    <th>Longitude</th>
+                    <th>Latitude</th>
+                    <th>Commune</th>
+                    <th>Cours d'eau</th>
+                    <th>Temperature en C°</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($temperatures as $temperature)
                 <tr>
                     <td>{{ $temperature->date_mesure_temp }}</td>
                     <td>{{ $temperature->longitude }}</td>
@@ -50,14 +47,15 @@
                     <td>
                         <a id="Suppression" class="icon" href="{{ route('temperature.supprimer', $temperature->id) }}"> <img src="/img/poubelle.png"></a>
                     </td>
-                    
+
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endif
+                @endforeach
+            </tbody>
+        </table>
+        @endif
     </div>
 
     @include('headfoot/footer')
 </body>
+
 </html>
--- resources/views/page/temperature/temp_form.blade.php
+++ resources/views/page/temperature/temp_form.blade.php
@@ -9,65 +9,68 @@
 
     <link rel="shortcut icon" type="image/x-icon" href="img/logo.png" />
 
-    <link rel="stylesheet" href="css/page_saisie_temp.css">
-    <link rel="stylesheet" href="css/footer.css"/>
+
     <link rel="stylesheet" href="css/header.css" />
+    <link rel="stylesheet" href="css/footer.css" />
+    <link rel="stylesheet" href="css/page_saisie_temp.css">
+
 </head>
 
 <body>
     @include('headfoot/header')
-    
-   
+
+
     <form action="{{ route('saisir_temp') }}" method="post">
-    @csrf
+        @csrf
 
-    <div class="form-group">
-        <label for="longitude">Longitude:</label>
-        <input type="number" class="form-control" id="longitude" name="longitude" required>
+        <div class="form-group">
+            <label for="longitude">Longitude:</label>
+            <input type="number" class="form-control" id="longitude" name="longitude" required>
+        </div>
+
+        <div class="form-group">
+            <label for="latitude">Latitude:</label>
+            <input type="number" class="form-control" id="latitude" name="latitude" required>
+        </div>
+
+        <div class="form-group">
+            <label for="libelle_commune">Commune:</label>
+            <input type="text" class="form-control" id="libelle_commune" name="libelle_commune" required>
+        </div>
+
+        <div class="form-group">
+            <label for="libelle_cours_eau">Cours d'eau:</label>
+            <select class="form-control" id="libelle_cours_eau" name="libelle_cours_eau" required>
+                <option value="">Select Cours d'eau</option>
+            </select>
+        </div>
+
+
+        <div class="form-group">
+            <label for="date_mesure_temp">Date mesure temp:</label>
+            <input type="date" class="form-control" id="date_mesure_temp" name="date_mesure_temp" required min="{{ date('Y-m-d') }}">
+        </div>
+
+        <div class="form-group">
+            <label for="heure_mesure_temp">Heure mesure temp:</label>
+            <input type="time" class="form-control" id="heure_mesure_temp" name="heure_mesure_temp" required>
+        </div>
+
+        <div class="form-group">
+            <label for="resultat">Temperature en C° :</label>
+            <input type="number" class="form-control" id="resultat" name="resultat" required>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Valider</button>
+    </form>
+
+    <div id="info">
+        <h5>Bienvenue {{ auth()->user()->name }} sur notre page de Température</h5>
+        <p>Cette page vous permet de saisir les données de température pour différents emplacements. Que vous soyez un météorologue, un chercheur en environnement ou simplement curieux des conditions météorologiques locales, notre outil de saisie de température vous offre une solution conviviale pour enregistrer et analyser les données de température.</p>
     </div>
 
-    <div class="form-group">
-        <label for="latitude">Latitude:</label>
-        <input type="number" class="form-control" id="latitude" name="latitude" required>
-    </div>
-
-    <div class="form-group">
-        <label for="libelle_commune">Commune:</label>
-        <input type="text" class="form-control" id="libelle_commune" name="libelle_commune" required>
-    </div>
-
-    <div class="form-group">
-        <label for="libelle_cours_eau">Cours d'eau:</label>
-        <select class="form-control" id="libelle_cours_eau" name="libelle_cours_eau" required>
-            <option value="">Select Cours d'eau</option>
-        </select>
-    </div>
-    
-
-    <div class="form-group">
-        <label for="date_mesure_temp">Date mesure temp:</label>
-        <input type="date" class="form-control" id="date_mesure_temp" name="date_mesure_temp" required min="{{ date('Y-m-d') }}">
-    </div>
-
-    <div class="form-group">
-        <label for="heure_mesure_temp">Heure mesure temp:</label>
-        <input type="time" class="form-control" id="heure_mesure_temp" name="heure_mesure_temp" required>
-    </div>
-
-    <div class="form-group">
-        <label for="resultat">Temperature en C° :</label>
-        <input type="number" class="form-control" id="resultat" name="resultat" required>
-    </div>
-    <br>
-    <button type="submit" class="btn btn-primary">Valider</button>
-</form>
-
-<div id="info">
-    <h5>Bienvenue {{ auth()->user()->name }} sur notre page de Température</h5>
-    <p>Cette page vous permet de saisir les données de température pour différents emplacements. Que vous soyez un météorologue, un chercheur en environnement ou simplement curieux des conditions météorologiques locales, notre outil de saisie de température vous offre une solution conviviale pour enregistrer et analyser les données de température.</p>
-</div>
-
-@include('headfoot/footer')
+    @include('headfoot/footer')
 
 </body>
+
 </html>
