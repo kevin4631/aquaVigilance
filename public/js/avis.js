52bf5304b71cf8e69c2b048b1279a5756127549c
kevin <kevinrochetaing46@gmail.com>
2024-04-09 03:53:08 +0200

    ajout js

--- public/js/avis.js
+++ public/js/avis.js
@@ -1,3 +1,23 @@
+function envoyerAvis() {
+    // Créer une instance de l'objet XMLHttpRequest
+    var xhr = new XMLHttpRequest();
+
+    // Spécifier la méthode HTTP et l'URL de la route à laquelle vous souhaitez envoyer la requête
+    xhr.open("GET", "/votre-route", true);
+
+    // Définir la fonction de rappel à exécuter lorsque la réponse est reçue
+    xhr.onreadystatechange = function () {
+        // Vérifier si la requête est terminée et que la réponse est prête
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // La réponse a été reçue avec succès
+            console.log("votre avis à bien etait envoyé");
+        }
+    };
+
+    // Envoyer la requête
+    xhr.send();
+}
+
 
 document.querySelector("#oui").addEventListener("click", function () {
     document.querySelector('#avis').style.display = 'none'
